a5cb0b6d7c23e2514d001c02b7b79962bfd610a6
Lukáš Drahník <ldrahnik@gmail.com>
2019-10-30 12:55:37 +0100

    WIP
    Signed-off-by: Lukáš Drahník <ldrahnik@gmail.com>

--- src/hpac.h
+++ src/hpac.h
@@ -15,24 +15,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "string.h"
-#include <pthread.h>
-#include <signal.h>
-#include <semaphore.h> 
-
-#define MAXTHREADS 10000
-
-/* arguments for pthread for algorithm */
-typedef struct pthread_args {
-  struct params* params;
-  struct graph* graph;
-  TVertex** vertex_out;
-  int vertex_out_count;
-} Tpthread_args;
 
 void cleanAll(TParams params, TGraph graph);
 int main(int argc, char *argv[]);
 int algorithm(TParams* params, TGraph* graph, int vertex_out_count, TVertex** vertex_out);
-void cleanThreadArg(Tpthread_args* threadarg);
-void cleanThreadArgs(Tpthread_args** threadargs, int thread_counts);
 
 #endif
